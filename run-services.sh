c62dcadb232e627745f50a273553ed79413b044b
v-r <vitali.rassolov@gmail.com>
2021-07-11 18:46:26 +0000

    add ES startup

--- run-services.sh
+++ run-services.sh
@@ -3,4 +3,5 @@
 service nginx start &
 /usr/sbin/php-fpm7.4 --fpm-config php-fpm.conf &
 redis-server &
+/home/gitpod/elasticsearch-6.8.9/bin/elasticsearch -d -p /home/gitpod/elasticsearch-6.8.9/pid -Ediscovery.type=single-node &
 supervisord &
